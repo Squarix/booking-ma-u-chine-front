d99e221d9e0d0cd8c417ef6de1e6750a360ac7f5
Uladzislau <uladzislau.bahdanouski@codex-soft.com>
2020-05-23 01:08:15 +0300

    Small fixes according to new fields names

--- src/Profile/Profile.jsx
+++ src/Profile/Profile.jsx
@@ -16,112 +16,112 @@ import Button from "@material-ui/core/Button";
 const profileService = new ProfileService();
 
 class Profile extends React.Component {
-	constructor(props) {
-		super(props)
-	}
+  constructor(props) {
+    super(props)
+  }
 
-	state = {
-		id: '',
-		firstName: '',
-		lastName: '',
-		phoneNumber: '',
-		editable: ''
-	}
+  state = {
+    id: '',
+    firstName: '',
+    lastName: '',
+    phoneNumber: '',
+    editable: ''
+  }
 
-	componentDidMount() {
-		profileService.getProfile(this.props.match.params.id).then(user => {
-			console.log(user)
-			this.setState({
-				firstName: user.first_name,
-				lastName: user.last_name,
-				phoneNumber: user.phoneNumber,
-				email: user.email,
-				id: user.id
-			})
-		})
-	}
+  componentDidMount() {
+    profileService.getProfile(this.props.match.params.id).then(user => {
+      console.log(user)
+      this.setState({...user})
+    })
+  }
 
-	handleSave = () => {
-		profileService.updateProfile(this.state.id, {
-			first_name: this.state.firstName,
-			last_name: this.state.lastName,
-			phoneNumber: this.state.phoneNumber
-		}).then(res => {console.log(res)})
-	}
+  handleSave = () => {
+    const {firstName, lastName, phoneNumber} = this.state;
+    profileService.updateProfile(this.state.id, {
+      firstName, lastName, phoneNumber
+    }).then(res => {
+      console.log(res)
+    })
+  }
 
-	handleChangeEditable(key){
-		this.setState({editable: key})
-	}
+  handleChangeEditable(key) {
+    this.setState({editable: key})
+  }
 
-	handleInputChange = (e) => {
-		this.setState({
-			[e.target.name]: e.target.value
-		})
-	}
+  handleInputChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
 
-	render() {
-		const {classes} = this.props
-		const editables = [
-			{key: 'Last name',    value: this.state.lastName,    state: 'lastName'},
-			{key: 'First name',   value: this.state.firstName,   state: 'firstName'},
-			{key: 'Phone number', value: this.state.phoneNumber, state: 'phoneNumber'},
-		]
-		return (
-			<React.Fragment>
-				<Container fixed>
-					<Grid container className={classes.main} spacing={4}>
-						{ editables.map(editable =>
-							<Grid xs={4} item className={classes.profileEntry}>
-								<label>{editable.key}</label>
-								<div className={classes.editable}>
-									{editable.state !== this.state.editable ?
-										<React.Fragment>
-											<Typography key={editable.key} variant={"h5"}>{editable.value || 'Not set'}</Typography>
-											<IconButton key={editable.state} aria-label="Edit" className={classes.margin} onClick={() => this.handleChangeEditable(editable.state)}>
-												<EditIcon fontSize="small"/>
-											</IconButton>
-										</React.Fragment>
-										:
-										<React.Fragment>
-											<TextField  key={editable.key}
-												id="standard-search"
-												name={editable.state}
-												label={editable.key}
-												value={editable.value || ''}
-												onChange={this.handleInputChange}
-												type="text"
-												className={classes.textField}
-												margin="normal"
-											/>
-											<IconButton key={editable.state} aria-label="Save" className={classes.margin} onClick={() => this.handleChangeEditable('')}>
-												<SaveIcon fontSize="small"/>
-											</IconButton>
-										</React.Fragment>
-									}
-								</div>
-							</Grid>
-						)}
-						<Grid xs={4} item>
-							<Button
-								variant="contained"
-								color="primary"
-								size="large"
-								className={classes.button}
-								startIcon={<SaveIcon />}
-								onClick={this.handleSave}
-							>
-								Save
-							</Button>
-						</Grid>
-						<Grid xs={12} item>
-							<Button href={'/profile/bookings'}>Bookings</Button>
-							<Button href={'/profile/rents'}>Rents</Button>
-						</Grid>
-					</Grid>
-				</Container>
-			</React.Fragment>
-		);
-	}
+  render() {
+    const {classes} = this.props
+    const editables = [
+      {key: 'Last name', value: this.state.lastName, state: 'lastName'},
+      {key: 'First name', value: this.state.firstName, state: 'firstName'},
+      {key: 'Phone number', value: this.state.phoneNumber, state: 'phoneNumber'},
+    ]
+    return (
+        <React.Fragment>
+          <Container fixed>
+            <Grid container className={classes.main} spacing={4}>
+              {editables.map(editable =>
+                  <Grid xs={4} item className={classes.profileEntry}>
+                    <label>{editable.key}</label>
+                    <div className={classes.editable}>
+                      {editable.state !== this.state.editable ?
+                          <React.Fragment>
+                            <Typography key={editable.key} variant={"h5"}>{editable.value || 'Not set'}</Typography>
+                            <IconButton
+                                key={editable.state} aria-label="Edit" className={classes.margin}
+                                onClick={() => this.handleChangeEditable(editable.state)}>
+                              <EditIcon fontSize="small"/>
+                            </IconButton>
+                          </React.Fragment>
+                          :
+                          <React.Fragment>
+                            <TextField
+                                key={editable.key}
+                                id="standard-search"
+                                name={editable.state}
+                                label={editable.key}
+                                value={editable.value || ''}
+                                onChange={this.handleInputChange}
+                                type="text"
+                                className={classes.textField}
+                                margin="normal"
+                            />
+                            <IconButton
+                                key={editable.state} aria-label="Save" className={classes.margin}
+                                onClick={() => this.handleChangeEditable('')}>
+                              <SaveIcon fontSize="small"/>
+                            </IconButton>
+                          </React.Fragment>
+                      }
+                    </div>
+                  </Grid>
+              )}
+              <Grid xs={4} item>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    size="large"
+                    className={classes.button}
+                    startIcon={<SaveIcon/>}
+                    onClick={this.handleSave}
+                >
+                  Save
+                </Button>
+              </Grid>
+              <Grid xs={12} item>
+                <Button href={'/profile/bookings'}>Bookings</Button>
+                <Button href={'/profile/rents'}>Rents</Button>
+              </Grid>
+            </Grid>
+          </Container>
+        </React.Fragment>
+    );
+  }
 }
 
 export default withStyles(styles, {withTheme: true})(Profile);
--- src/Room/ViewRoom.jsx
+++ src/Room/ViewRoom.jsx
@@ -30,231 +30,230 @@ import Menu from "../Layouts/Menu";
 const roomService = new RoomService();
 
 class ViewRoom extends React.Component {
-	constructor(props) {
-		super(props);
-		const today = new Date();
-		today.setHours(0, 0, 0, 0);
-		this.state = {
-			address: '',
-			bookedDates: [today.toDateString()],
-			city: '',
-			description: '',
-			filters: [],
-			images: [],
-			guestsamount: 0,
-			size: 0,
-			todayprice: 0,
-			useremail: '',
-			startDate: null,
-			endDate: null,
-			totalPrice: null,
-			redirectCabinet: false,
+  constructor(props) {
+    super(props);
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+    this.state = {
+      address: '',
+      bookedDates: [today.toDateString()],
+      city: '',
+      description: '',
+      filters: [],
+      images: [],
+      guestsAmount: 0,
+      size: 0,
+      todayPrice: 0,
+      user: '',
+      startDate: null,
+      endDate: null,
+      totalPrice: null,
+      redirectCabinet: false,
 
-			message: '',
-			dialogOpen: false,
-			title: 'Booking Info'
-		}
-	}
+      message: '',
+      dialogOpen: false,
+      title: 'Booking Info'
+    }
+  }
 
-	onStartDateChange = (date) => {
-		this.handleDateChanged(date, null);
-	};
+  onStartDateChange = (date) => {
+    this.handleDateChanged(date, null);
+  };
 
-	handleDialogClose = () => {
-		this.setState({
-			dialogOpen: false
-		})
-	};
+  handleDialogClose = () => {
+    this.setState({
+      dialogOpen: false
+    })
+  };
 
-	handleDateChanged = (newStartDate, newEndDate) => {
-		const start = newStartDate ? newStartDate : this.state.startDate;
-		const end = newEndDate ? newEndDate : this.state.endDate;
-		const price = this.state.todayprice;
-		let days = 0;
+  handleDateChanged = (newStartDate, newEndDate) => {
+    const start = newStartDate ? newStartDate : this.state.startDate;
+    const end = newEndDate ? newEndDate : this.state.endDate;
+    const price = this.state.todayprice;
+    let days = 0;
 
-		if (end & start) {
-			days = getDifferenceInDays(start, end);
-			console.log(days);
-		}
+    if (end & start) {
+      days = getDifferenceInDays(start, end);
+      console.log(days);
+    }
 
-		this.setState({
-			totalPrice: days * price,
-			startDate: start,
-			endDate: end
-		})
-	}
+    this.setState({
+      totalPrice: days * price,
+      startDate: start,
+      endDate: end
+    })
+  }
 
-	onEndDateChange = (date) => {
-		this.handleDateChanged(null, date);
-	}
+  onEndDateChange = (date) => {
+    this.handleDateChanged(null, date);
+  }
 
-	componentDidMount() {
-		const roomId = this.props.match.params.id;
-		roomService.getRoomBookings(roomId).then(bookings => {
-			console.log(bookings)
-			this.setState({
-				bookedDates: bookings
-			})
-		});
-		roomService.getRoom(roomId).then(room => {
-			console.log(room);
-			this.setState(room);
-		});
-	}
+  componentDidMount() {
+    const roomId = this.props.match.params.id;
+    roomService.getRoomBookings(roomId).then(bookings => {
+      console.log(bookings)
+      this.setState({
+        bookedDates: bookings
+      })
+    });
+    roomService.getRoom(roomId).then(room => {
+      this.setState(room);
+    });
+  }
 
-	handleBooking = () => {
-		console.log('booking in proccess');
-		const roomId = this.props.match.params.id;
-		const params = {
-			roomId: roomId,
-			startDate: this.state.startDate,
-			endDate: this.state.endDate
-		};
+  handleBooking = () => {
+    console.log('booking in proccess');
+    const roomId = this.props.match.params.id;
+    const params = {
+      roomId: roomId,
+      startDate: this.state.startDate,
+      endDate: this.state.endDate
+    };
 
-		let message;
-		roomService.bookRoom(params)
-			.then(res => {
-				message = res.message;
-			})
-			.catch(e => {
-				e.response.json()
-					.then(data => {
-						this.setState({
-							message: data.message
-						});
-					})
-			})
-			.finally(() => {
-				this.setState({
-					message: message,
-					dialogOpen: true
-				})
-			})
-	}
+    let message;
+    roomService.bookRoom(params)
+        .then(res => {
+          message = res.message;
+        })
+        .catch(e => {
+          e.response.json()
+              .then(data => {
+                this.setState({
+                  message: data.message
+                });
+              })
+        })
+        .finally(() => {
+          this.setState({
+            message: message,
+            dialogOpen: true
+          })
+        })
+  }
 
-	render() {
-		const {classes} = this.props;
+  render() {
+    const {classes} = this.props;
 
-		return (
-			<React.Fragment>
-				<Menu/>
-				<Container fixed>
-					<Grid container className={classes.bodyContainer}>
-						<Grid md={9} xs={12} className={classes.content} item>
-							<Typography variant={'h1'} className={classes.header}>{this.state.address}</Typography>
-							<Carousel className={classes.carousel}>
-								{this.state.images.map(image =>
-									<div className={classes.roomImageDiv}>
-										<img src={`${apiUrl}/${image}`} className={classes.roomImage}/>
-									</div>
-								)}
-							</Carousel>
-							<div className={classes.descriptionBlock}>
-								<Typography variant={'h4'}>Description</Typography>
-								<p className={classes.description}>
-									{this.state.description}
-								</p>
-							</div>
-						</Grid>
-						<InfoDialog
-							open={this.state.dialogOpen}
-							message={this.state.message}
-							title={this.state.title}
-							handleClose={this.handleDialogClose}
-						/>
-						<Grid xs={12} md={3} className={classes.sidebars} item>
-							<List>
-								<ListItem className={classes.label}>
-									<GroupIcon/>
-									<span>{'max '}{this.state.guestsamount} guests</span>
-								</ListItem>
-								<ListItem className={classes.label}>
-									<AttachMoneyIcon/>
-									<span>{' ~'}{this.state.todayprice} per day</span>
-								</ListItem>
-								<ListItem className={classes.label}>
-									<LocationOnIcon/>
-									<span>{this.state.city}</span>
-								</ListItem>
-								<ListItem className={classes.label}>
-									<PermIdentityIcon/>
-									<span>{this.state.useremail}</span>
-								</ListItem>
-								<ListItem className={classes.label}>
-									<DatePicker
-										name={'startDate'}
-										selected={this.state.startDate}
-										className={classes.datePicker}
-										onChange={this.onStartDateChange}
-										placeholderText='Select start date'
-										dateFormat={'d MMMM yyyy'}
-										filterDate={(date) => isValidDate(this.state.bookedDates, date, null, this.state.endDate)}
-									/>
-								</ListItem>
-								<ListItem className={classes.label}>
-									<DatePicker
-										name={'endDate'}
-										className={classes.datePicker}
-										selected={this.state.endDate}
-										onChange={this.onEndDateChange}
-										placeholderText='Select end date'
-										dateFormat={'d MMMM yyyy'}
-										filterDate={(date) => isValidDate(this.state.bookedDates, date, this.state.startDate, null)}
-									/>
-								</ListItem>
-								{this.state.totalPrice ?
-									<React.Fragment>
-										<ListItem className={classes.label}>
-											<span>Total: {this.state.totalPrice} $</span>
-										</ListItem>
-										<ListItem className={classes.label}>
-											<Button onClick={this.handleBooking} variant="contained" color="primary"
-											        className={classes.button}>
-												Book
-											</Button>
-										</ListItem>
-									</React.Fragment> : ''
-								}
-							</List>
-						</Grid>
-						{
-							this.state.redirectCabinet ?
-								<Redirect to={'/profile/bookings'}/> : ''
-						}
+    return (
+        <React.Fragment>
+          <Menu/>
+          <Container fixed>
+            <Grid container className={classes.bodyContainer}>
+              <Grid md={9} xs={12} className={classes.content} item>
+                <Typography variant={'h1'} className={classes.header}>{this.state.address}</Typography>
+                <Carousel className={classes.carousel}>
+                  {this.state.images.map(image =>
+                      <div className={classes.roomImageDiv}>
+                        <img src={`${apiUrl}/${image.imagePath}`} className={classes.roomImage}/>
+                      </div>
+                  )}
+                </Carousel>
+                <div className={classes.descriptionBlock}>
+                  <Typography variant={'h4'}>Description</Typography>
+                  <p className={classes.description}>
+                    {this.state.description}
+                  </p>
+                </div>
+              </Grid>
+              <InfoDialog
+                  open={this.state.dialogOpen}
+                  message={this.state.message}
+                  title={this.state.title}
+                  handleClose={this.handleDialogClose}
+              />
+              <Grid xs={12} md={3} className={classes.sidebars} item>
+                <List>
+                  <ListItem className={classes.label}>
+                    <GroupIcon/>
+                    <span>{'max '}{this.state.guestsAmount} guests</span>
+                  </ListItem>
+                  <ListItem className={classes.label}>
+                    <AttachMoneyIcon/>
+                    <span>{' ~'}{this.state.todayPrice} per day</span>
+                  </ListItem>
+                  <ListItem className={classes.label}>
+                    <LocationOnIcon/>
+                    <span>{this.state.city.name}</span>
+                  </ListItem>
+                  <ListItem className={classes.label}>
+                    <PermIdentityIcon/>
+                    <span>{this.state.user.email}</span>
+                  </ListItem>
+                  <ListItem className={classes.label}>
+                    <DatePicker
+                        name={'startDate'}
+                        selected={this.state.startDate}
+                        className={classes.datePicker}
+                        onChange={this.onStartDateChange}
+                        placeholderText='Select start date'
+                        dateFormat={'d MMMM yyyy'}
+                        filterDate={(date) => isValidDate(this.state.bookedDates, date, null, this.state.endDate)}
+                    />
+                  </ListItem>
+                  <ListItem className={classes.label}>
+                    <DatePicker
+                        name={'endDate'}
+                        className={classes.datePicker}
+                        selected={this.state.endDate}
+                        onChange={this.onEndDateChange}
+                        placeholderText='Select end date'
+                        dateFormat={'d MMMM yyyy'}
+                        filterDate={(date) => isValidDate(this.state.bookedDates, date, this.state.startDate, null)}
+                    />
+                  </ListItem>
+                  {this.state.totalPrice ?
+                      <React.Fragment>
+                        <ListItem className={classes.label}>
+                          <span>Total: {this.state.totalPrice} $</span>
+                        </ListItem>
+                        <ListItem className={classes.label}>
+                          <Button onClick={this.handleBooking} variant="contained" color="primary"
+                                  className={classes.button}>
+                            Book
+                          </Button>
+                        </ListItem>
+                      </React.Fragment> : ''
+                  }
+                </List>
+              </Grid>
+              {
+                this.state.redirectCabinet ?
+                    <Redirect to={'/profile/bookings'}/> : ''
+              }
 
-					</Grid>
+            </Grid>
 
-				</Container>
-				<Footer/>
-			</React.Fragment>
-		)
-	}
+          </Container>
+          <Footer/>
+        </React.Fragment>
+    )
+  }
 }
 
 function isValidDate(bookedDates, date, startDate, endDate) {
-	let minDate = new Date();
-	minDate.setHours(0, 0, 0, 0);
+  let minDate = new Date();
+  minDate.setHours(0, 0, 0, 0);
 
-	let maxDate = new Date();
-	maxDate.setFullYear(maxDate.getFullYear() + 1);
+  let maxDate = new Date();
+  maxDate.setFullYear(maxDate.getFullYear() + 1);
 
-	minDate = startDate ? startDate : minDate;
-	maxDate = endDate ? endDate : maxDate;
+  minDate = startDate ? startDate : minDate;
+  maxDate = endDate ? endDate : maxDate;
 
-	return (checkDate(bookedDates, date) && (date > minDate) && (date < maxDate))
+  return (checkDate(bookedDates, date) && (date > minDate) && (date < maxDate))
 }
 
 function checkDate(dates, searchDate) {
-	for (const date of dates) {
-		if (new Date(date.startdate) <= searchDate && searchDate <= new Date(date.enddate))
-			return false
-	}
+  for (const date of dates) {
+    if (new Date(date.startdate) <= searchDate && searchDate <= new Date(date.enddate))
+      return false
+  }
 
-	return true;
+  return true;
 }
 
 function getDifferenceInDays(startDate, endDate) {
-	return (endDate - startDate) / (24 * 60 * 60 * 1000)
+  return (endDate - startDate) / (24 * 60 * 60 * 1000)
 }
 
 export default withStyles(styles, {withTheme: true})(ViewRoom);
